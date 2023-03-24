36592f53e9672aa079f85e606e03308522b8745b
= <alex.assuied@sanofi.com>
2023-03-24 12:33:41 +0100

    delete duplicated library(dplyr) and package::

--- R/fda-table_32.R
+++ R/fda-table_32.R
@@ -1,32 +1,30 @@
 # Pre-processing
-
-library(dplyr)
 library(dplyr)
 library(scda)
 library(tern)
 
 # Safety population
 adsl <-
-  scda::synthetic_cdisc_dataset("rcd_2022_06_27", "adsl") %>%
-  tern::df_explicit_na() %>%
-  dplyr::filter(SAFFL == "Y")
+  synthetic_cdisc_dataset("rcd_2022_06_27", "adsl") %>%
+  df_explicit_na() %>%
+  filter(SAFFL == "Y")
 
 # Vital signs - DIABP
 advs <-
-  scda::synthetic_cdisc_dataset("rcd_2022_06_27", "advs") %>%
-  tern::df_explicit_na() %>%
-  dplyr::filter(PARAMCD == "DIABP" & SAFFL == "Y" & AVISITN >= 1) %>%
-  dplyr::group_by(USUBJID, PARAMCD) %>%
-  dplyr::mutate(maxDIABP = max(AVAL)) %>%
+  synthetic_cdisc_dataset("rcd_2022_06_27", "advs") %>%
+  df_explicit_na() %>%
+  filter(PARAMCD == "DIABP" & SAFFL == "Y" & AVISITN >= 1) %>%
+  group_by(USUBJID, PARAMCD) %>%
+  mutate(maxDIABP = max(AVAL)) %>%
   ungroup() %>%
-  dplyr::mutate(
+  mutate(
     L60 = (maxDIABP < 60),
     U60 = (maxDIABP > 60),
     U90 = (maxDIABP > 90),
     U110 = (maxDIABP > 110),
     UE120 = (maxDIABP >= 120)
   ) %>%
-  formatters::var_relabel(
+  var_relabel(
     L60 = "<60",
     U60 = ">60",
     U90 = ">90",
@@ -36,9 +34,9 @@ advs <-
 
 # Build layout
 lyt <-
-  rtables::basic_table(show_colcounts = TRUE) %>%
-  rtables::split_cols_by("ARM") %>%
-  tern::count_patients_with_flags(
+  basic_table(show_colcounts = TRUE) %>%
+  split_cols_by("ARM") %>%
+  count_patients_with_flags(
     var = "USUBJID",
     flag_variables = var_labels(advs[, c("L60", "U60", "U90", "U110", "UE120")])
   )
@@ -50,15 +48,15 @@ result <- build_table(lyt, df = advs, alt_counts_df = adsl)
 # Add titles/footnotes
 
 ## Top left header
-rtables::top_left(result) <- "Diastolic Blood Pressure\n(Pa)"
+top_left(result) <- "Diastolic Blood Pressure\n(Pa)"
 
 ## Title
-formatters::main_title(result) <-
+main_title(result) <-
   "Table 32. Percentage of Patients With Maximum Diastolic Blood Pressure by Category of Blood
 Pressure Postbaseline, Safety Population, Pooled Analysis"
 
 ## Footnotes
-formatters::main_footer(result) <- c(
+main_footer(result) <- c(
   "Source: [include Applicant source, datasets and/or software tools used].",
   "Abbreviations: CI, confidence interval; n, number of patients with indicated blood pressure;
   N, number of patients in treatment arm with available blood pressure data"
