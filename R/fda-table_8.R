fcee29feb94d1e059376c6c3834b224400295b6c
Alex Assuied <123543888+aassuied-ps@users.noreply.github.com>
2023-04-28 15:34:13 +0200

    Update R/fda-table_8.R
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>

--- R/fda-table_8.R
+++ R/fda-table_8.R
@@ -49,7 +49,7 @@ tbl_join <-
 final_list <-
   as_listing(
     tbl_join,
-    key_cols = c("ACTARM", "USUBJID", "AGESEX", "DOSAGE", "DOSDUR"),
+    key_cols = c("ACTARM", "USUBJID", "AGESEX", "DOSAGE", "DOSDUR", "DTHADY"),
     disp_cols = NULL,
     main_title = "Table 8. All Individual Patient Deaths, Safety Population, Pooled Analyses (1)",
     main_footer =
