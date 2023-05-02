1d6fdf9a35588ced857bd943381945417390478b
Alex Assuied <123543888+aassuied-ps@users.noreply.github.com>
2023-05-02 14:56:38 +0200

    23 fda table 8 (#47)
    * first commit
    
    * Ready for pull request.
    
    * [skip actions] Restyle files
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Same replacement as above
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    True, I forgot that.
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_8.R
    
    Understood
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Delete AEDECOD and AETERM from var_relabel
    
    * Update R/fda-table_8.R
    
    Done
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * [skip actions] Restyle files
    
    * Replace <Missing> by blank (df_explicic_na())
    
    * Rename file - 8 to 08
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    Co-authored-by: Emily de la Rua <emily.de_la_rua@contractors.roche.com>

---
+++ R/fda-table_08.R
@@ -0,0 +1,60 @@
+# Pre-processing
+library(dplyr)
+library(scda)
+library(tern)
+library(rlistings)
+
+# Deaths
+adae <-
+  synthetic_cdisc_dataset("rcd_2022_06_27", "adae") %>%
+  filter(SAFFL == "Y" & AESDTH == "Y") %>%
+  select(
+    STUDYID, USUBJID, ACTARM, ACTARMCD, AGE, SEX, DTHCAT, DTHCAUS, ASTDY, AESDTH, AEOUT, DTHADY
+  ) %>%
+  mutate(
+    AGESEX = paste0(AGE, "/", SEX),
+    DTHADY = as.character(DTHADY)
+  ) %>%
+  distinct(USUBJID, DTHCAT, DTHCAUS, DTHADY, .keep_all = TRUE)
+
+# Dosing
+adex <-
+  synthetic_cdisc_dataset("rcd_2022_06_27", "adex") %>%
+  filter(PARAMCD == "TDOSE") %>%
+  select(USUBJID, PARAMCD, PARAM, AVAL, AVALU, TRTSDT, TRTEDT) %>%
+  mutate(
+    DOSDUR = (TRTEDT - TRTSDT + 1) %>% as.character(),
+    DOSAGE = paste0(AVAL, " ", AVALU)
+  )
+
+# Join
+tbl_join <-
+  adae %>%
+  left_join(adex, by = "USUBJID") %>%
+  select(ACTARM, USUBJID, AGESEX, DOSAGE, DOSDUR, DTHADY, DTHCAUS, DTHCAT) %>%
+  var_relabel(
+    ACTARM = "Study Arm",
+    USUBJID = "Patient ID",
+    AGESEX = "Age/\nGender",
+    DOSAGE = "Dosage",
+    DTHADY = "Study\nDay of\nDeath",
+    DOSDUR = "Dosing\nDuration\n(Days)",
+    DTHCAUS = "Cause of Death\nMedDRA\nPreferred Term",
+    DTHCAT = "Cause of Death\nVerbatim Term"
+  ) %>%
+  df_explicit_na(na_level = "") %>%
+  arrange(ACTARM, USUBJID, AGESEX, DOSAGE, DOSDUR, DTHADY)
+
+# Listing
+final_list <-
+  as_listing(
+    tbl_join,
+    key_cols = c("ACTARM", "USUBJID", "AGESEX", "DOSAGE", "DOSDUR", "DTHADY"),
+    main_title = "Table 8. All Individual Patient Deaths, Safety Population, Pooled Analyses (1)",
+    main_footer =
+      "Source: [include Applicant source, datasets and/or software tools used].
+(1) Duration = [e.g., X week double-blind treatment period or median and a range indicating pooled trial durations].
+Abbreviations: MedDRA, Medical Dictionary for Regulatory Activities; PT, preferred term"
+  )
+
+head(final_list)
