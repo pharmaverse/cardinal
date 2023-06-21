2d2ab8e072000d4e0a6b9336bdc24bf50ad7b9b0
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-06-21 18:05:39 -0400

    Update after merging in main

--- R/fda-table_14.R
+++ R/fda-table_14.R
@@ -1,4 +1,5 @@
-#' FDA Table 14: Patients With Adverse Events by System Organ Class and FDA Medical Query, Safety Population, Pooled Analyses
+#' FDA Table 14: Patients With Adverse Events by System Organ Class and FDA Medical Query, Safety Population,
+#'   Pooled Analyses
 #'
 #' @details
 #' * `adae` must contain the variables `SAFFL`, `USUBJID`, `AEBODSYS`, and the variables specified by `arm_var`,
