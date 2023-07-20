9a829f7506cb54e25685a52b5a683a11b4260ca0
Emily de la Rua <emily.de_la_rua@contractors.roche.com>
2023-07-20 16:13:49 -0400

    Finish table 2 with tplyr

--- R/fda-table_02.R
+++ R/fda-table_02.R
@@ -77,39 +77,38 @@ make_table_02 <- function(df,
   tbl
 }
 
-#' @describeIn make_table_02 Create FDA table 2 using functions from `Tplyr`.
+#' @describeIn make_table_02 Create FDA table 2 using functions from `Tplyr` and `tfrmt`.
+#'
+#' @param tplyr_raw (`logical`)\cr whether the raw `tibble` created using `Tplyr` functions should be returned, or the
+#'   output returned should be formatted using functions from `tfrmt` (default).
+#'
+#' @note
+#' * `make_table_02_tplyr` does not currently support footnote annotations
+#' * `make_table_02_tplyr` does not currently support `alt_counts_df` when `tplyr_raw = TRUE`.
 #'
 #' @return
-#' * `make_table_02_tplyr` returns a `tibble` object.
+#' * `make_table_02_tplyr` returns a `gt_tbl` object when `tplyr_raw = FALSE` (default) and
+#'   a `tibble` object when `tplyr_raw = TRUE`.
 #'
 #' @examples
-#' library(huxtable)
-#'
 #' tbl <- make_table_02_tplyr(df = adsl)
+#' tbl
 #'
-#' # format output table using huxtable
-#' tbl <- huxtable::as_hux(tbl, add_colnames = FALSE) %>%
-#'   huxtable::set_bold(1, 1:ncol(tbl), FALSE) %>%
-#'   huxtable::set_align(1, 1:ncol(tbl), "center") %>%
-#'   huxtable::set_align(2:nrow(tbl), 3:ncol(tbl), "center") %>%
-#'   huxtable::set_valign(1, 1:ncol(tbl), "bottom") %>%
-#'   huxtable::set_bottom_border(1, 1:ncol(tbl), 1) %>%
-#'   huxtable::set_width(2) %>%
-#'   huxtable::set_col_width(c(.2, .3, .125, .125, .125, .125))
-#'
-#' huxtable::position(tbl) <- "left"
-#'
+#' tbl <- make_table_02_tplyr(df = adsl, tplyr_raw = TRUE)
 #' tbl
 #'
 #' @export
 make_table_02_tplyr <- function(df,
+                                alt_counts_df = NULL,
                                 show_colcounts = TRUE,
                                 arm_var = "ARM",
                                 vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY"),
                                 lbl_vars = formatters::var_labels(df, fill = TRUE)[vars],
                                 lbl_overall = "Total Population",
                                 na_rm = FALSE,
-                                prune_0 = TRUE) {
+                                prune_0 = TRUE,
+                                tplyr_raw = FALSE,
+                                annotations = NULL) {
   checkmate::assert_subset(c("SAFFL", vars, arm_var), names(df))
   assert_flag_variables(df, "SAFFL")
 
@@ -122,52 +121,129 @@ make_table_02_tplyr <- function(df,
 
   lyt <- tplyr_table(df, treat_var = !!sym(arm_var), where = SAFFL == "Y")
 
-  if (!is.null(lbl_overall)) lyt <- lyt %>% add_total_group()
+  if (!is.null(lbl_overall)) lyt <- lyt %>% add_total_group(group_name = lbl_overall)
 
   for (var in vars) {
     var_lbl <- lbl_vars[[var]]
     if (var_types[[var]] == "numeric") {
-      lyt <- lyt %>%
-        add_layer(
+      if (tplyr_raw) {
+        lyt <- lyt %>% add_layer(
           group_desc(vars(!!sym(var)), by = !!sym(var_lbl)) %>%
             set_format_strings(
               "Mean (SD)" = f_str("xx.x (xx.x)", mean, sd),
               "Median (Min - Max)" = f_str("xx.x (xx.x - xx.x)", median, min, max)
             )
         )
-    } else {
-      lyt <- lyt %>%
-        add_layer(
-          group_count(vars(!!sym(var)), by = !!sym(var_lbl)) %>%
-            set_format_strings(f_str("xx (xx%)", n, pct))
+      } else {
+        lyt <- lyt %>% add_layer(
+          group_desc(vars(!!sym(var)), by = !!sym(var_lbl)) %>%
+            set_format_strings(
+              "Mean" = f_str("xx.xxxx", mean), "SD" = f_str("xx.xxxx", sd), "Median" = f_str("xx.xxxx", median),
+              "Min" = f_str("xx.xxxx", min), "Max" = f_str("xx.xxxx", max)
+            )
         )
+      }
+    } else {
+      if (tplyr_raw) {
+        lyt <- lyt %>% add_layer(
+          group_count(vars(!!sym(var)), by = !!sym(var_lbl)) %>%
+            set_format_strings(f_str("xx (xx.x%)", n, pct))
+        )
+      } else {
+        lyt <- lyt %>% add_layer(
+          group_count(vars(!!sym(var)), by = !!sym(var_lbl)) %>%
+            set_format_strings(f_str("xx;xx.xxxx", n, pct))
+        )
+      }
     }
   }
-
   tbl <- lyt %>% build()
-
-  tbl <- tbl %>%
-    arrange(ord_layer_index, ord_layer_1, ord_layer_2) %>%
-    apply_row_masks(row_breaks = TRUE) %>%
-    select(starts_with("row_label"), starts_with("var1_")) %>%
-    add_column_headers(
-      paste0(
-        "Characteristic | | ",
-        paste0(levels(df[[arm_var]]), if (show_colcounts) paste0("\n(N=**", levels(df[[arm_var]]), "**)| ") else "| ", collapse = ""),
-        ifelse(!is.null(lbl_overall), paste0(lbl_overall, ifelse(show_colcounts, c("\n(N=**Total**)"), ""), ""))
-      ),
-      header_n = header_n(lyt)
-    )
 
   if (na_rm) {
     na_ind <- tbl[, 2] != "<Missing>"
     tbl <- tbl[na_ind, ]
   }
 
-  if (prune_0) {
-    prune_ind <- apply(tbl, MARGIN = 1, function(x) all(x == "") || !all(gsub("[0()\\% ]", "", x[-c(1:2)]) == ""))
-    tbl <- tbl[prune_ind, ]
-  }
+  if (tplyr_raw) {
+    tbl <- tbl %>%
+      arrange(ord_layer_index, ord_layer_1, ord_layer_2) %>%
+      apply_row_masks(row_breaks = TRUE) %>%
+      select(starts_with("row_label"), starts_with("var1_")) %>%
+      add_column_headers(
+        paste0(
+          "Characteristic | | ",
+          paste0(
+            levels(df[[arm_var]]), if (show_colcounts) paste0("\n(N=**", levels(df[[arm_var]]), "**)| ") else "| ",
+            collapse = ""
+          ),
+          ifelse(!is.null(lbl_overall), paste0(lbl_overall, ifelse(show_colcounts, c("\n(N=**Total**)"), ""), ""))
+        ),
+        header_n = header_n(lyt)
+      )
 
+    if (prune_0) {
+      prune_ind <- apply(tbl, MARGIN = 1, function(x) all(x == "") || !all(gsub("[0()\\% ]", "", x[-c(1:2)]) == ""))
+      tbl <- tbl[prune_ind, ]
+    }
+  } else {
+    tbl <- tbl %>%
+      tidyr::pivot_longer(head(names(.)[-c(1:2)], -3), names_to = "column", values_to = "value") %>%
+      mutate(
+        column = gsub("var1_", "", column),
+        param = ifelse(row_label2 %in% c("Mean", "SD", "Median", "Min", "Max"), row_label2, "n;pct"),
+        row_label2 = case_when(
+          row_label2 %in% c("Mean", "SD") ~ "Mean (SD)",
+          row_label2 %in% c("Median", "Min", "Max") ~ "Median (Min - Max)",
+          .default = row_label2
+        )
+      ) %>%
+      tidyr::separate_rows(c("param", "value"), sep = ";") %>%
+      mutate(value = as.numeric(value))
+
+    if (show_colcounts) {
+      colcounts <- summary(if (!is.null(alt_counts_df)) alt_counts_df[[arm_var]] else df[[arm_var]])
+      big_ns <- tibble(
+        column = c(levels(df[[arm_var]]), lbl_overall),
+        param = "bigN",
+        value = if (!is.null(lbl_overall)) c(colcounts, sum(colcounts)) else colcounts,
+      )
+      tbl <- bind_rows(tbl, big_ns)
+    }
+
+    if (prune_0) {
+      tbl <- tbl %>%
+        group_by(ord_layer_index, ord_layer_2) %>%
+        filter(sum(value) > 0) %>%
+        ungroup()
+    }
+
+    tbl <- tfrmt(
+      group = row_label1,
+      label = row_label2,
+      column = column,
+      param = param,
+      value = value,
+      title = annotations[["title"]],
+      subtitle = paste(annotations[["subtitles"]], collapse = ", "),
+      sorting_cols = c(ord_layer_1, ord_layer_2),
+      body_plan = body_plan(
+        frmt_structure(
+          group_val = ".default", label_val = ".default",
+          frmt_combine("{n} ({pct}%)", n = frmt("xx"), pct = frmt_when("==0" ~ "", TRUE ~ frmt("xx.x")))
+        ),
+        frmt_structure(
+          group_val = ".default", label_val = "Mean (SD)",
+          frmt_combine("{Mean} ({SD})", Mean = frmt("xx.x"), SD = frmt("xx.x"))
+        ),
+        frmt_structure(
+          group_val = ".default", label_val = "Median (Min - Max)",
+          frmt_combine("{Median} ({Min} - {Max})", Median = frmt("xx.x"), Min = frmt("xx.x"), Max = frmt("xx.x"))
+        )
+      ),
+      col_plan = col_plan(-starts_with("ord")),
+      big_n = if (show_colcounts) big_n_structure(param_val = "bigN", n_frmt = frmt("\n(N=xx)")) else NULL
+    ) %>%
+      print_to_gt(tbl)
+  }
   tbl
 }
