4d2b89f72fbfb90fc816b644e57d2e321a211a66
aassuied-ps <123543888+aassuied-ps@users.noreply.github.com>
2023-10-18 15:34:29 +0200

    lint test-table_32.R

--- tests/testthat/test-table_32.R
+++ tests/testthat/test-table_32.R
@@ -65,9 +65,11 @@ test_that("Table 32 (gtsum) generation works with default values", {
   expect_snapshot(res)
 })
 
-
 test_that("Table 32 (gtsum) generation works with custom values", {
-  result <- suppressWarnings(make_table_32_gtsum(advs = advs, adsl = adsl, lbl_overall = "Total Population") %>% as_gt())
+  result <- suppressWarnings(
+    make_table_32_gtsum(advs = advs, adsl = adsl, lbl_overall = "Total Population") %>%
+      as_gt()
+  )
 
   res <- expect_silent(as.data.frame(result))
   expect_snapshot(res)
