8c0db4e109515fad3690644cd53531d7a449343f
Alex Assuied <123543888+aassuied-ps@users.noreply.github.com>
2024-01-04 12:11:18 +0100

    176 new table table 35 (#182)
    * Create fda-table_35.R
    
    Create function file.
    
    * Create test-table_35.R
    
    Test file ran and table_35.md was created.
    
    * Create template-table_35.qmd
    
    Add table_35.png
    
    Did not succeed to run make_table_35 during the test of quarto page (function not found).
    
    * Update _quarto.yml
    
    * Update NEWS.md
    
    Also run generate-template_index.R
    
    * Add a blank line on append_toleft
    
    Tests snapshots updated
    
    * [skip actions] Restyle files
    
    * Update R/fda-table_35.R
    
    Add soc_var
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_35.R
    
    Spaces before title
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_35.R
    
    Replace count_patients_with_flag() by count_occurences()
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * Update R/fda-table_35.R
    
    Use sort_at_path
    
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * Push
    
    Update template (add soc_var and lbl_soc_var), function, tests (add a test on soc_var and lbl_soc_var)
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * Update AEBODSYS to soc_var in @details
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    * [skip actions] Roxygen Man Pages Auto Update
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Emily de la Rua <59304861+edelarua@users.noreply.github.com>
    Co-authored-by: 27856297+dependabot-preview[bot]@users.noreply.github.com <27856297+dependabot-preview[bot]@users.noreply.github.com>

---
+++ R/fda-table_35.R
@@ -0,0 +1,62 @@
+#' Table 35. Patients With Adverse Events1 by System Organ Class,
+#'   Safety Population, Pooled Analysis (or Trial X)
+#'
+#' @details
+#' * `adae` must contain the variables specified by
+#'   `arm_var`, `id_var`, `soc_var` and `saffl_var`.
+#' * If specified, `alt_counts_df` must contain the variables specified by `arm_var`, `id_var`, and `saffl_var`.
+#' * Columns are split by arm. Overall population column is excluded by default (see `lbl_overall` argument).
+#' * All-zero rows are removed by default (see `prune_0` argument).
+#'
+#' @inheritParams argument_convention
+#'
+#' @examples
+#' library(dplyr)
+#'
+#' adae <- scda::synthetic_cdisc_dataset("rcd_2022_10_13", "adae")
+#' adsl <- scda::synthetic_cdisc_dataset("rcd_2022_10_13", "adsl")
+#' tbl <- make_table_35(adae = adae, alt_counts_df = adsl)
+#' tbl
+#'
+#' @export
+make_table_35 <- function(adae,
+                          alt_counts_df = NULL,
+                          show_colcounts = TRUE,
+                          id_var = "USUBJID",
+                          arm_var = "ARM",
+                          saffl_var = "SAFFL",
+                          soc_var = "AESOC",
+                          lbl_soc_var = formatters::var_labels(adae, fill = TRUE)[soc_var],
+                          lbl_overall = NULL,
+                          risk_diff = NULL,
+                          prune_0 = FALSE,
+                          annotations = NULL) {
+  checkmate::assert_subset(c(soc_var, arm_var, id_var, saffl_var), names(adae))
+  assert_flag_variables(adae, saffl_var)
+
+  adae <- adae %>%
+    filter(.data[[saffl_var]] == "Y") %>%
+    arrange(soc_var) %>%
+    df_explicit_na()
+
+  alt_counts_df <-
+    alt_counts_df_preproc(alt_counts_df, id_var, arm_var, saffl_var)
+
+  lyt <- basic_table_annot(show_colcounts, annotations) %>%
+    split_cols_by_arm(arm_var, lbl_overall, risk_diff) %>%
+    count_occurrences(
+      vars = soc_var,
+      drop = FALSE,
+      riskdiff = !is.null(risk_diff)
+    ) %>%
+    append_topleft(c("", lbl_soc_var))
+
+  tbl <- build_table(lyt, df = adae, alt_counts_df = alt_counts_df) %>%
+    sort_at_path(
+      path = c(soc_var),
+      scorefun = score_occurrences_cols(col_names = levels(adae[[arm_var]]))
+    )
+  if (prune_0) tbl <- prune_table(tbl)
+
+  tbl
+}
