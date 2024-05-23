c49c66ec0e0287c18bba8e173a47010c112419b7
ayogasekaram <abinaya.yogasekaram@contractors.roche.com>
2024-05-23 17:31:19 +0000

    initial commit - create make_table_02 ARD function.

--- R/fda-table_02.R
+++ R/fda-table_02.R
@@ -1,7 +1,7 @@
 #' FDA Table 2: Baseline Demographic and Clinical Characteristics, Safety Population, Pooled Analyses
 #'
 #' @details
-#' * `df` must contain the variables specified by `vars`, `arm_var`, and `saffl_var`.
+#' * `data` must contain the variables specified by `continuous_vars`, and `categorical_vars`.
 #' * If specified, `alt_counts_df` must contain the variables specified by `arm_var`, `id_var`, and `saffl_var`.
 #' * Flag variables (i.e. `XXXFL`) are expected to have two levels: `"Y"` (true) and `"N"` (false). Missing values in
 #'   flag variables are treated as `"N"`.
@@ -18,7 +18,8 @@ NULL
 #' @describeIn make_table_02 Create FDA table 2 using functions from `rtables` and `tern`.
 #'
 #' @return
-#' * `make_table_02` returns an `rtable` object.
+#' * `make_table_02` returns an object matching the selected `tbl_engine` argument.
+#' The intermediary `ARD` object is also returned by default
 #'
 #' @examples
 #' library(dplyr)
@@ -32,21 +33,105 @@ NULL
 #'   )) %>% formatters::with_label("Age Group, years")) %>%
 #'   formatters::var_relabel(AGE = "Age, years")
 #'
-#' tbl <- make_table_02(df = adsl)
+#' tbl <- make_table_02(data = adsl)
 #' tbl
 #'
 #' @export
-make_table_02 <- function(df,
-                          alt_counts_df = NULL,
-                          show_colcounts = TRUE,
-                          arm_var = "ARM",
-                          saffl_var = "SAFFL",
-                          vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY"),
-                          lbl_vars = formatters::var_labels(df, fill = TRUE)[vars],
-                          lbl_overall = "Total Population",
-                          na_rm = FALSE,
-                          prune_0 = TRUE,
-                          annotations = NULL) {
+
+make_table_02 <- function(data,
+                          ...,
+                          tbl_engine = "gtsummary",
+                          return_ard = TRUE) {
+  # check data viability
+  assert_subset(continuous_vars, names(data))
+  assert_subset(categorical_vars, names(data))
+
+  if (tbl_engine == "rtables") {
+    ard <- "ARD not available for {rtables}"
+    tbl <- make_table_02_rtables(df = data)
+  } else {
+    ard <- make_ard_02(...)
+    # commenting out this table building portion until package refactor
+    # if (tbl_engine == "gtsummary") {
+    #   tbl <- make_table_02_gtsum(ard, ...)
+    # } else {
+    #   tbl <- make_table_02_tplyr(ard, ...)
+    # }
+  }
+
+  if (return_ard) {
+    res <- list(ard = ard)
+  } else {
+    res <- tbl
+  }
+}
+
+
+make_ard_02 <- function(data = data,
+                        by = "ARM",
+                        continuous_vars = c("AGE"),
+                        categorical_vars = c("SEX", "AGEGR1", "RACE", "ETHNIC")) {
+  ard <- cards::ard_stack(
+    data = data,
+    by = by,
+    cards::ard_continuous(
+      variables = continuous_vars,
+      statistic = ~ continuous_summary_fns(c("mean", "sd", "median", "min", "max"))
+    ),
+    cards::ard_categorical(variables = categorical_vars)
+  )
+
+  return(ard)
+}
+
+
+#' @details
+#' * `df` must contain the variables specified by `continuous_vars`, and `categorical_vars`.
+#' * If specified, `alt_counts_df` must contain the variables specified by `arm_var`, `id_var`, and `saffl_var`.
+#' * Flag variables (i.e. `XXXFL`) are expected to have two levels: `"Y"` (true) and `"N"` (false). Missing values in
+#'   flag variables are treated as `"N"`.
+#' * Columns are split by arm. Overall population column is included by default (see `lbl_overall` argument).
+#' * Information from either ADSUB or ADVS is generally included into `df` prior to analysis.
+#' * Numbers in table for non-numeric variables represent the absolute numbers of patients and fraction of `N`.
+#' * All-zero rows are removed by default (see `prune_0` argument).
+#'
+#' @inheritParams argument_convention
+#'
+#' @name make_table_02
+NULL
+
+#' @describeIn make_table_02 Create FDA table 2 using functions from `rtables` and `tern`.
+#'
+#' @return
+#' * `make_table_02` returns an object matching the selected `tbl_engine` argument.
+#' The intermediary `ARD` object is also returned by default
+#'
+#' @examples
+#' library(dplyr)
+#'
+#' adsl <- random.cdisc.data::cadsl %>%
+#'   mutate(AGEGR1 = as.factor(case_when(
+#'     AGE >= 17 & AGE < 65 ~ ">=17 to <65",
+#'     AGE >= 65 ~ ">=65",
+#'     AGE >= 65 & AGE < 75 ~ ">=65 to <75",
+#'     AGE >= 75 ~ ">=75"
+#'   )) %>% formatters::with_label("Age Group, years")) %>%
+#'   formatters::var_relabel(AGE = "Age, years")
+#'
+#' tbl <- make_table_02(data = adsl)
+#' tbl
+#' @keywords Internal
+make_table_02_rtables <- function(df,
+                                  alt_counts_df = NULL,
+                                  show_colcounts = TRUE,
+                                  arm_var = "ARM",
+                                  saffl_var = "SAFFL",
+                                  vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY"),
+                                  lbl_vars = formatters::var_labels(df, fill = TRUE)[vars],
+                                  lbl_overall = "Total Population",
+                                  na_rm = FALSE,
+                                  prune_0 = TRUE,
+                                  annotations = NULL) {
   assert_subset(c(vars, arm_var, saffl_var), names(df))
   assert_flag_variables(df, saffl_var)
 
@@ -91,7 +176,7 @@ make_table_02 <- function(df,
 #' tbl <- make_table_02_tplyr(df = adsl)
 #' tbl
 #'
-#' @export
+#' @keywords Internal
 make_table_02_tplyr <- function(df,
                                 alt_counts_df = NULL,
                                 show_colcounts = TRUE,
@@ -255,7 +340,7 @@ make_table_02_tplyr <- function(df,
 #' tbl <- make_table_02_gtsum(df = adsl)
 #' tbl
 #'
-#' @export
+#' @keywords Internal
 make_table_02_gtsum <- function(df,
                                 show_colcounts = TRUE,
                                 arm_var = "ARM",
