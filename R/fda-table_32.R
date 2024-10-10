19c0e879118f4cdc5205193ba1734b0c309cafb9
aassuied-ps <123543888+aassuied-ps@users.noreply.github.com>
2024-10-10 18:07:54 +0200

    up

--- R/fda-table_32.R
+++ R/fda-table_32.R
@@ -175,9 +175,14 @@ ard_table_32 <- function(df,
 #'
 #' @examples
 #' adsl <- random.cdisc.data::cadsl
+#' advs <- random.cdisc.data::cadvs
 #'
 #' # gtsummary table --------------
-#' ard <- cardinal:::ard_table_32(df = adsl)
+#' ard <- cardinal:::ard_table_32(
+#'   df = advs,
+#'   alt_counts_df = adsl,
+#'   subset = (PARAMCD == "DIABP" & AVISITN >= 1)
+#' )
 #' tbl_gtsummary <- cardinal::make_table_32_gtsummary(
 #'   df = advs,
 #'   alt_counts_df = adsl,
