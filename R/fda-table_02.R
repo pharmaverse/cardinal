f0daa13caa3c01a1c69c04a8d765e6fe7424c693
ayogasekaram <abinaya.yogasekaram@contractors.roche.com>
2024-11-11 17:09:01 +0000

    update tests, update spelling, update quarto page

--- R/fda-table_02.R
+++ R/fda-table_02.R
@@ -12,7 +12,7 @@ NULL
 #' @describeIn make_table_02 Create FDA table 2 using an ARD.
 #'
 #' @return
-#' * `make_table_02` returns a list containing a gtsummary table object.
+#' * `make_table_02` returns a list containing a `gtsummary` table object.
 #' The intermediary `ARD` object can also be returned with `return_ard` set to `TRUE`.
 #'
 #' @examples
@@ -35,7 +35,7 @@ make_table_02 <- function(data,
                           show_colcounts = TRUE,
                           arm_var = "ARM",
                           saffl_var = "SAFFL",
-                          vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY"),
+                          vars = c("SEX", "AGE", "RACE", "ETHNIC", "COUNTRY"),
                           lbl_vars = formatters::var_labels(data, fill = TRUE)[vars],
                           lbl_overall = "Total Population",
                           na_rm = FALSE,
@@ -70,7 +70,7 @@ make_table_02 <- function(data,
     gtsummary::modify_header(all_stat_cols() ~ "**{level}**  \nN = {n}") %>%
     gtsummary::add_overall(last = TRUE, col_label = paste0("**", lbl_overall, "**  \nN = {n}")) %>%
     gtsummary::add_stat_label(label = all_continuous2() ~ c("Mean (SD)", "Median (min - max)")) %>%
-    gtsummary::modify_footnote(update = everything() ~ NA) %>%
+    gtsummary::modify_footnote(everything() ~ NA) %>%
     gtsummary::modify_column_alignment(columns = all_stat_cols(), align = "right")
 
   if (return_ard) {
--- tests/testthat/test-fda-table_02.R
+++ tests/testthat/test-fda-table_02.R
@@ -3,152 +3,28 @@ adsl <- adsl_raw %>%
   df_explicit_na()
 
 test_that("Table 02 generation works with default values", {
-  result <- make_table_02_rtables(adsl)
+  options(pillar.print_max = 50, width = 200)
 
-  res <- expect_silent(result)
-  expect_snapshot(res)
-})
+  result <- make_table_02(adsl)
+  expect_snapshot(result$tbl |> as.data.frame())
+  })
 
 test_that("Table 02 generation works with custom values", {
+  options(pillar.print_max = 50, width = 200)
+
   advs <- advs_raw %>%
     dplyr::filter(AVISIT == "BASELINE", VSTESTCD == "TEMP") %>%
-    dplyr::select("USUBJID", "AVAL")
+    dplyr::select(all_of(c("USUBJID", "AVAL")))
 
   anl <- dplyr::left_join(adsl, advs, by = "USUBJID") %>% df_explicit_na()
 
-  result <- make_table_02_rtables(
+  result <- make_table_02(
     anl,
-    vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY", "AVAL"),
-    lbl_vars = c(
-      "Sex", "Age, years", "Age Group, years", "Race", "Ethnicity",
-      "Country of Participation", "Baseline Temperature (C)"
-    ),
-    na_rm = TRUE,
-    annotations = list(
-      title = "Table 2. Baseline Demographic and Clinical Characteristics Safety Population, Pooled Analyses",
-      main_footer = "Source: [include Applicant source, datasets and/or software tools used].",
-      prov_footer = paste(
-        "Abbreviations: N, number of patients in treatment arm;",
-        "n, number of patients with given characteristic; SD, standard deviation"
-      )
-    )
+    vars = c("SEX", "AGE", "RACE", "ETHNIC", "COUNTRY", "AVAL")
   )
 
   res <- expect_silent(result)
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation works with some NA values", {
-  set.seed(1)
-  adsl[sample(seq_len(nrow(adsl)), 50), "SEX"] <- NA
-
-  adsl <- adsl %>% df_explicit_na()
-
-  result <- make_table_02_rtables(adsl, vars = "SEX", lbl_vars = "Sex")
-
-  res <- expect_silent(result)
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (tplyr) works with default values", {
-  options(pillar.print_max = 50, width = 200)
-
-  result <- withr::with_options(
-    opts_partial_match_old,
-    make_table_02_tplyr(adsl)
-  )
-
-  res <- expect_silent(result[["_data"]])
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (tplyr) works with `tplyr_raw` = TRUE", {
-  options(pillar.print_max = 50, width = 200)
-
-  result <- withr::with_options(
-    opts_partial_match_old,
-    make_table_02_tplyr(adsl, tplyr_raw = TRUE)
-  )
-
-  res <- expect_silent(result)
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (tplyr) works with custom values", {
-  options(pillar.print_max = 50, width = 200)
-
-  advs <- advs_raw %>%
-    dplyr::filter(AVISIT == "BASELINE", VSTESTCD == "TEMP") %>%
-    dplyr::select("USUBJID", "AVAL")
-
-  anl <- dplyr::left_join(adsl, advs, by = "USUBJID") %>% df_explicit_na()
-
-  result <- withr::with_options(
-    opts_partial_match_old,
-    make_table_02_tplyr(
-      anl,
-      vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY", "AVAL"),
-      lbl_vars = c(
-        "Sex", "Age, years", "Age Group, years", "Race", "Ethnicity",
-        "Country of Participation", "Baseline Temperature (C)"
-      ),
-      na_rm = TRUE,
-      annotations = list(
-        title = "Table 2. Baseline Demographic and Clinical Characteristics Safety Population, Pooled Analyses"
-      ),
-      prune_0 = FALSE
-    )
-  )
-
-  res <- expect_silent(result[["_data"]])
-  expect_snapshot(res)
-
-  res <- expect_silent(result[["_heading"]])
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (tplyr) works with some NA values", {
-  options(pillar.print_max = 50, width = 200)
-
-  set.seed(1)
-  adsl[sample(seq_len(nrow(adsl)), 50), "SEX"] <- NA
-
-  adsl <- adsl %>% df_explicit_na()
-
-  result <- withr::with_options(
-    opts_partial_match_old,
-    make_table_02_tplyr(adsl, vars = "SEX", lbl_vars = "Sex")
-  )
-
-  res <- expect_silent(result[["_data"]])
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (gtsum) works with default values", {
-  options(pillar.print_max = 50, width = 200)
-
-  result <- suppressWarnings(make_table_02_gtsum(adsl) %>% gt::extract_body())
-
-  res <- expect_silent(result)
-  expect_snapshot(res)
-})
-
-test_that("Table 02 generation (gtsum) works with custom values", {
-  options(pillar.print_max = 50, width = 200)
-
-  advs <- advs_raw %>%
-    dplyr::filter(AVISIT == "BASELINE", VSTESTCD == "TEMP") %>%
-    dplyr::select("USUBJID", "AVAL")
-
-  anl <- dplyr::left_join(adsl, advs, by = "USUBJID") %>% df_explicit_na()
-
-  result <- suppressWarnings(make_table_02_gtsum(
-    anl,
-    vars = c("SEX", "AGE", "AGEGR1", "RACE", "ETHNIC", "COUNTRY", "AVAL")
-  ) %>% gt::extract_body())
-
-  res <- expect_silent(result)
-  expect_snapshot(res)
+  expect_snapshot(result$tbl |> as.data.frame())
 })
 
 test_that("Table 02 generation (gtsum) works with some NA values", {
@@ -159,8 +35,8 @@ test_that("Table 02 generation (gtsum) works with some NA values", {
 
   adsl <- adsl %>% df_explicit_na()
 
-  result <- suppressWarnings(make_table_02_gtsum(adsl, vars = "SEX") %>% gt::extract_body())
+  result <- suppressWarnings(make_table_02(adsl, vars = "SEX"))
 
   res <- expect_silent(result)
-  expect_snapshot(res)
+  expect_snapshot(result$tbl |> as.data.frame())
 })
