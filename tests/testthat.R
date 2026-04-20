2feb5cfc38389880448a06979edecbbcc88e3aa0
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-04-20 12:31:52 +0200

    Don't test if testthat is not present

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,8 @@
-library(testthat)
+if (requireNamespace("testthat", quietly = TRUE)) {
+  library(testthat)
 
-pkg_name <- "cardinal"
-library(pkg_name, character.only = TRUE)
-testthat::test_check(pkg_name)
+  pkg_name <- "cardinal"
+  library(pkg_name, character.only = TRUE)
+  testthat::test_check(pkg_name)
+}
+
