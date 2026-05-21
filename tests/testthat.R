dd35a051ae677159a1980332fc615c8a2694cfc7
Lluís Revilla <lluis.revilla_sancho@roche.com>
2026-05-21 10:18:00 +0200

    Detect new warnings

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,6 @@
 if (requireNamespace("testthat", quietly = TRUE)) {
   library(testthat)
-
+  options(warn = 2) # Warnings become errors
   pkg_name <- "cardinal"
   library(pkg_name, character.only = TRUE)
   testthat::test_check(pkg_name)
